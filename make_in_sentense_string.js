b5ef62a53b2e5abcdf9a51380c7421c2c70058e1
noobow <skywind.hk34@gmail.com>
2022-07-27 13:10:44 +0900

    Create make_in_sentense_string.js

---
+++ make_in_sentense_string.js
@@ -0,0 +1,23 @@
+Main();
+ 
+function Main()
+{
+	var cnt = GetLineCount(0);
+	var all = "";
+	for (var i = 1; i <= cnt; i++) {
+		var line = GetLineStr(i).replace("\r","").replace("\n","");
+		if(i == 1){
+			all += ("('" + line + "'");
+		}else if(i == cnt){
+			all += (",'" + line + "')");
+		}else{
+			all += (",'" + line + "'");
+		}
+		all += "\r\n";
+	}
+	
+	SelectAll(0);
+	InsText(all);
+	SelectAll(0);
+	Editor.Copy();;
+}
